af29e09e01518bd8a4a00653936c81e65922f93b
mramgir <mramgir@192.168.1.13>
2019-07-27 15:38:42 +0530

    MR: Very first commit. Just Created Plugin Project

---
+++ com.everythingjava.ejperformanceanalyzer/src/com/everythingjava/ejperformanceanalyzer/parts/TraceView.java
@@ -0,0 +1,75 @@
+/*
+ * EJ Performance Analyzer
+ * Author: Mayur Ramgir
+ * Supported By: EverythingJava - www.everythingjava.com
+ */
+package com.everythingjava.ejperformanceanalyzer.parts;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class TraceView {
+	private Label myLabelInView;
+
+	@PostConstruct
+	public void createPartControl(Composite parent) {
+		myLabelInView = new Label(parent, SWT.BORDER);
+		myLabelInView.setText("Welcome to EJ Performance Analyzer");
+	}
+
+	@Focus
+	public void setFocus() {
+		myLabelInView.setFocus();
+
+	}
+
+
+	/**
+	 * This method manages the selection of your current object. In this example
+	 * we listen to a single Object (even the ISelection already captured in E3
+	 * mode). <br/>
+	 * You should change the parameter type of your received Object to manage
+	 * your specific selection
+	 * 
+	 * @param o
+	 *            : the current object received
+	 */
+	@Inject
+	@Optional
+	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object o) {
+
+		// Remove the 2 following lines in pure E4 mode, keep them in mixed mode
+		if (o instanceof ISelection) // Already captured
+			return;
+
+		// Test if label exists (inject methods are called before PostConstruct)
+		if (myLabelInView != null)
+			myLabelInView.setText("Current single selection class is : " + o.getClass());
+	}
+
+	/**
+	 * This method manages the multiple selection of your current objects. <br/>
+	 * You should change the parameter type of your array of Objects to manage
+	 * your specific selection
+	 * 
+	 * @param o
+	 *            : the current array of objects received in case of multiple selection
+	 */
+	@Inject
+	@Optional
+	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object[] selectedObjects) {
+
+		// Test if label exists (inject methods are called before PostConstruct)
+		if (myLabelInView != null)
+			myLabelInView.setText("This is a multiple selection of " + selectedObjects.length + " objects");
+	}
+}
